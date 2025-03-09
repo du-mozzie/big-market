55bd6ff2822c8678f84e5fd0c0d597ffa9b32b80
mozzie <du.mozzie@outlook.com>
2025-03-09 15:38:17 +0800

    feat: 重命名

--- docs/dev-ops/mysql/sql/mysql.sql
+++ docs/dev-ops/mysql/sql/mysql.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 127.0.0.1
- Source Server Type    : MySQL
- Source Server Version : 50639
- Source Host           : localhost:3306
- Source Schema         : road-map
-
- Target Server Type    : MySQL
- Target Server Version : 50639
- File Encoding         : 65001
-
- Date: 15/07/2023 09:26:39
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
